3e6e4bd60eb0acdedfbcba0e47aecb219653e9ae
CHANGMIN LEE <changmin812@gmail.com>
2026-08-12 20:43:44 +0900

    [level 2] Title: 짝지어 제거하기, Time: 59.45 ms, Memory: 13.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = 0
+    stack = []
+    for i in s :
+        if stack and stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    
+    if stack == [] :
+        answer = 1
+    return answer
